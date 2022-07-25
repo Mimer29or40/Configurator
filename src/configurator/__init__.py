aff0812730251029943d742155b479b3d040b149
Ryan <>
2022-07-25 14:20:57 -0400

    Added settings class

--- src/configurator/__init__.py
+++ src/configurator/__init__.py
@@ -1,5 +1,12 @@
 import importlib.metadata
 import importlib.resources
+import json
+import logging
+import pprint
+from dataclasses import asdict
+from dataclasses import dataclass
+from dataclasses import field
+from dataclasses import fields
 from pathlib import Path
 from typing import Dict, Final, Optional
 
@@ -39,3 +46,38 @@ __project_urls__: Final[Dict[str, str]] = {
 __copyright__: Final[str] = f"Copyright 2022 {__author__}"
 
 __data_dir__: Final[Path] = (Path(__file__).parent / "data").resolve()
+
+logger = logging.getLogger(__title__)
+
+
+@dataclass
+class Settings:
+    version: int = 1
+    last_settings_file: Path = field(
+        default=Path("instance_settings.json"), metadata={"convert": Path}
+    )
+
+    @classmethod
+    def load(cls, file: Path) -> None:
+        if not file.exists():
+            logger.info("Generating Default Settings")
+            file.parent.mkdir(parents=True, exist_ok=True)
+            file.write_text(json.dumps(asdict(cls()), indent=4))
+
+        # This may error out if the json is malformed
+        loaded: Dict[str, str] = json.loads(file.read_text())
+
+        for cls_field in fields(cls):
+            metadata = cls_field.metadata
+            if cls_field.name not in loaded or loaded[cls_field.name] == "":
+                logger.warning(
+                    f"Config is missing '{cls_field.name}'. Defaulting to '{cls_field.default}'"
+                )
+                loaded[cls_field.name] = cls_field.default
+            if "convert" in metadata:
+                loaded[cls_field.name] = metadata["convert"](loaded[cls_field.name])
+
+        for key, value in loaded.items():
+            setattr(cls, key, value)
+
+        logger.info("Config Loaded:\n%s", pprint.pformat(cls(**loaded)))
--- src/configurator/__main__.py
+++ src/configurator/__main__.py
@@ -1,4 +1,23 @@
+import logging
+import sys
+import traceback
+from typing import Any, Optional
+
 import configurator
 
+
+def main(*argv: str) -> Optional[Any]:
+    try:
+        print(argv)
+        return 0
+    except Exception as e:
+        logger = logging.getLogger(configurator.__title__)
+        logger.critical(
+            f"An Exception caused the program to exit:"
+            f"\n{''.join(traceback.format_exception(e))}"
+        )
+        return 1
+
+
 if __name__ == "__main__":
-    print(f"Running Package:\n\t{configurator.__name__}")
+    sys.exit(main(*sys.argv[1:]))
